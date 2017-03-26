200202422e55198b6761badc71384acba9199c86
Will Schumacher <meintodengel@gmail.com>
2017-03-25 19:09:37 -0700

    update reqs

--- server/seed.py
+++ server/seed.py
@@ -17,9 +17,9 @@ def ingest_data(data):
         crime = Crime(val[0], val[1], val[2], val[3], val[4], val[5], val[6], val[7], val[8], val[9], val[10])
 
         i += 1
-        print i
         if i % 1000 == 0:
-            print $'${i}%'
+            p = i / 700000.
+            print str(p) + '%'
 
         db.session.add(crime)
         db.session.commit()
